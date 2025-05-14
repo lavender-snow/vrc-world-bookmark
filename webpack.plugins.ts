c8d8278fcc4d70d670b1f795d6a9ed4396ac91b2
lavender-snow <lavender32192@gmail.com>
2025-05-14 22:37:49 +0900

    [追加] アプリ名やバージョン情報を環境変数に追加

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,8 +1,14 @@
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import type { WebpackPluginInstance } from 'webpack';
+import { DefinePlugin, type WebpackPluginInstance } from 'webpack';
+import packageJson from './package.json';
 
 export const plugins: WebpackPluginInstance[] = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
+  new DefinePlugin({
+    "process.env.APP_NAME": JSON.stringify(packageJson.name),
+    "process.env.APP_VERSION": JSON.stringify(packageJson.version),
+    "process.env.REPOSITORY": JSON.stringify(packageJson.repository?.url),
+  })
 ];
