b5da7ca1d77e6a1f219b3fe7d39d1d4d4524d864
lavender-snow <lavender32192@gmail.com>
2025-05-18 19:16:12 +0900

    [変更] ウィンドウサイズ変更

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,7 +14,7 @@ if (electronSquirrelStartup) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
+    height: 760,
     width: 800,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
