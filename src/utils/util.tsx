d1099e1afabcac0a5e69939de6867cef33ec3248
lavender-snow <lavender32192@gmail.com>
2025-05-22 22:15:29 +0900

    [追加] クリップボードに書き込むutil関数を追加

--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -4,13 +4,17 @@
 // https://vrchat.com/home/world/wrld_xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/info
 // https://vrchat.com/home/launch?worldId=wrld_xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx&instanceId=00000~private(usr_xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)~canRequestInvite~region(jp)~nonce(xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)
 export function getWorldId(urlOrId: string): string | null {
-    // wrld_xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx形式の文字列を抽出
-    const regex = /wrld_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+  // wrld_xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx形式の文字列を抽出
+  const regex = /wrld_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
 
-    const match = urlOrId.match(regex);
-    if (match) {
-        return match[0];
-    } else {
-        return null;
-    }
+  const match = urlOrId.match(regex);
+  if (match) {
+    return match[0];
+  } else {
+    return null;
+  }
+}
+
+export function writeClipboard(text: string) {
+  navigator.clipboard.writeText(text);
 }
