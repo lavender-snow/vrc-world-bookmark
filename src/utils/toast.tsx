302a60de5479b18dcd86b4ccd43eef66ff0f7660
lavender-snow <lavender32192@gmail.com>
2025-05-24 16:46:39 +0900

    [追加] トースト通知に種類を追加

--- src/utils/toast.tsx
+++ src/utils/toast.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import styles from "./toast.scss";
 import { parseSassTime } from "./util";
+import classNames from "classnames";
+import { NoticeType } from "../consts/const";
 
-export function Toast({ message, onClose }: { message: string, onClose: () => void }) {
+export function Toast({ message, onClose, noticeType = NoticeType.info }: { message: string, onClose: () => void, noticeType?: NoticeType }) {
   // Sassで定義した時間を基にトーストの表示時間を計算
   const displayTime = parseSassTime(styles.FADEIN) + parseSassTime(styles.FADEIN) + parseSassTime(styles.VISIBLE);
   useEffect(() => {
@@ -12,6 +14,6 @@ export function Toast({ message, onClose }: { message: string, onClose: () => vo
   }, [onClose]);
   if (!message) return null;
   return (
-    <div className={styles.toast}>{message}</div>
+    <div className={classNames(styles.toast, styles[noticeType])}>{message}</div>
   );
 }
