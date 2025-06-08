3df0b55b79675edb78430e07810fa9b6d548b5ed
lavender-snow <lavender32192@gmail.com>
2025-06-09 00:37:32 +0900

    [変更] トースト通知が複数表示可能に変更

---
+++ src/contexts/toast-provider.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from 'react';
+
+import { NoticeType } from 'src/consts/const';
+import { ToastList } from 'src/utils/toast-list';
+
+type ToastContext = {
+  addToast: (message: string, type?: NoticeType) => void;
+};
+
+interface toast {
+  id: string;
+  message: string;
+  noticeType: NoticeType;
+}
+
+const ToastContext = createContext<ToastContext | undefined>(undefined);
+
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+  const [toasts, setToasts] = useState<toast[]>([]);
+
+  function addToast(message: string, type: NoticeType = NoticeType.info) {
+    setToasts(toasts => [...toasts, { id: crypto.randomUUID(), message, noticeType: type }]);
+  };
+
+  function removeToast(id: string) {
+    setToasts(toasts => toasts.filter(toast => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ addToast }}>
+      {children}
+      <ToastList toasts={toasts} removeToast={removeToast} />
+    </ToastContext.Provider>
+  );
+}
+
+export function useToast() {
+  const ctx = useContext(ToastContext);
+  if (!ctx) throw new Error('useToast must be used within a ToastProvider');
+  return ctx;
+}
