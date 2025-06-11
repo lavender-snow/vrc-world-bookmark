b080cd5cfd5059ca468e8e7699d86f1c4755a4ac
lavender-snow <lavender32192@gmail.com>
2025-06-12 02:58:24 +0900

    [変更] ワールド登録時に入力したURLに対するフィードバックが行われるように変更

--- src/react-components/common/input-text.tsx
+++ src/react-components/common/input-text.tsx
@@ -7,17 +7,23 @@ export function InputText({
   onChange,
   placeholder,
   className = '',
+  onKeyDown,
+  onBlur,
 }: {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   className?: string;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
 }) {
   return (
     <input
       value={value}
       type="text"
       onChange={onChange}
+      onKeyDown={(e) => onKeyDown && onKeyDown(e)}
+      onBlur={onBlur}
       placeholder={placeholder}
       className={classNames([styles.inputText, className])}
     />
