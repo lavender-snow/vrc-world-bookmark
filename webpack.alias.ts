d9453ad5b4a46aadc394007dd1efa5dca0c676e8
lavender-snow <lavender32192@gmail.com>
2025-06-15 01:03:05 +0900

    [修正] パス参照を修正

--- webpack.alias.ts
+++ webpack.alias.ts
@@ -5,5 +5,5 @@ import type { ResolveOptions } from 'webpack';
 export const alias: Required<ResolveOptions>['alias'] = {
   src: path.resolve(__dirname, './src'),
   assets: path.resolve(__dirname, './assets'),
-  commonComponents: path.resolve(__dirname, 'src/react-components/common'),
+  commonComponents: path.resolve(__dirname, './src/react-components/common'),
 };
