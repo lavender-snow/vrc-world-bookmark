99690dc73328cebd7fb1dfc663a15b91ad577a37
lavender-snow <lavender32192@gmail.com>
2025-06-16 23:00:14 +0900

    [追加] Reactコンポーネントをテストするための設定を追加

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
-import { createDefaultPreset } from 'ts-jest';
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-export const testEnvironment = 'node';
-export const transform = {
-  ...tsJestTransformCfg,
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
 };
