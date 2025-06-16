d72be1a5313766c9c48137eff75b948021e08ee8
lavender-snow <lavender32192@gmail.com>
2025-06-16 21:48:01 +0900

    [追加] util関数のテストを追加

---
+++ src/utils/util.test.ts
@@ -0,0 +1,80 @@
+import {
+  getWorldId,
+  debounce,
+  checkTag,
+} from './util';
+
+describe('getWorldId', () => {
+  it('ワールドID自体からワールドIDを抽出出来ることのテスト', () => {
+    const id = 'wrld_15fe33f1-937e-4e93-8a40-902fb9552a11';
+    expect(getWorldId(id)).toBe(id);
+  });
+
+  it('ワールドURLからワールドIDを抽出出来ることのテスト', () => {
+    const url = 'https://vrchat.com/home/world/wrld_15fe33f1-937e-4e93-8a40-902fb9552a11/info';
+    expect(getWorldId(url)).toBe('wrld_15fe33f1-937e-4e93-8a40-902fb9552a11');
+  });
+
+  it('インスタンスURLからワールドIDを抽出出来ることのテスト', () => {
+    const url = 'https://vrchat.com/home/launch?worldId=wrld_15fe33f1-937e-4e93-8a40-902fb9552a11&instanceId=00000';
+    expect(getWorldId(url)).toBe('wrld_15fe33f1-937e-4e93-8a40-902fb9552a11');
+  });
+
+  it('無関係のURLからワールドIDが抽出出来ないことのテスト', () => {
+    expect(getWorldId('https://t.co/yy6NORT1SL')).toBeNull();
+  });
+
+  it('欠落しているワールドIDがワールドIDとして抽出されないことのテスト', () => {
+    expect(getWorldId('wrld_15fe33f1-937e-4e93-8a40-902fb9552a1')).toBeNull();
+  });
+});
+
+describe('debounce', () => {
+  jest.useFakeTimers();
+
+  it('発火が抑制されることをテスト', () => {
+    const fn = jest.fn();
+    const debounced = debounce(fn, 100);
+
+    debounced('a');
+    debounced('b');
+    debounced('c');
+
+    expect(fn).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(100);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith('c');
+  });
+});
+
+describe('checkTag', () => {
+  it('チルタグが判定できることをテスト', () => {
+    expect(checkTag('author_tag_chill').isChill).toBe(true);
+    expect(checkTag('AUTHOR_TAG_CHILL').isChill).toBe(true);
+  });
+
+  it('ホラータグが判定できることをテスト', () => {
+    expect(checkTag('author_tag_horror').isHorror).toBe(true);
+  });
+
+  it('ゲームタグが判定できることをテスト', () => {
+    expect(checkTag('author_tag_game').isGame).toBe(true);
+    expect(checkTag('author_tag_riddle').isGame).toBe(true);
+  });
+
+  it('管理者タグが判定できることをテスト', () => {
+    expect(checkTag('admin_foo').isAdmin).toBe(true);
+    expect(checkTag('user_foo').isAdmin).toBe(false);
+  });
+
+  it('無関係のタグがfalseになることをテスト', () => {
+    const check = checkTag('other');
+
+    expect(check.isChill).toBe(false);
+    expect(check.isHorror).toBe(false);
+    expect(check.isGame).toBe(false);
+    expect(check.isAdmin).toBe(false);
+  });
+});
