b6e8c1d901b1ecb447d02dcdc59248d5c174262a
lavender-snow <lavender32192@gmail.com>
2025-06-18 00:17:17 +0900

    [整理] インポートを整理

--- src/react-components/settings/world-update-progress.test.tsx
+++ src/react-components/settings/world-update-progress.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor ,act } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import {act} from 'react';
 
 import { WorldUpdateProgress } from './world-update-progress';
 
