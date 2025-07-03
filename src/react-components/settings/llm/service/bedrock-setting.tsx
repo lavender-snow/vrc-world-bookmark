82262f6470e1b12f1d7214c6fd0a8d13c6b0a20c
lavender-snow <lavender32192@gmail.com>
2025-07-03 20:09:00 +0900

    [変更] 設定値をAppDataProviderから取得するように変更

--- src/react-components/settings/llm/service/bedrock-setting.tsx
+++ src/react-components/settings/llm/service/bedrock-setting.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import styles from './bedrock-setting.scss';
 
@@ -6,6 +6,7 @@ import { Button } from 'commonComponents/button';
 import { InputText } from 'commonComponents/input-text';
 import { AWS_REGION_ID, AWS_REGIONS, AwsRegionId } from 'src/consts/aws';
 import { NoticeType } from 'src/consts/const';
+import { useAppData } from 'src/contexts/app-data-provider';
 import { useToast } from 'src/contexts/toast-provider';
 import { DropDownList } from 'src/react-components/common/drop-down-list';
 import { SettingItem } from 'src/react-components/settings/setting-item';
@@ -17,7 +18,7 @@ const defaultCredentials: BedrockCredentials = {
 };
 
 export function BedrockSetting() {
-  const [credentialsSaved, setCredentialsSaved] = useState(false);
+  const { llmEnabled, setLLMEnabled } = useAppData();
   const [credentials, setCredentials] = useState<BedrockCredentials>(defaultCredentials);
   const { addToast } = useToast();
 
@@ -43,23 +44,17 @@ export function BedrockSetting() {
   };
 
   const handleSave = () => {
-    setCredentialsSaved(true);
+    setLLMEnabled(true);
     window.credentialStore.saveKey('bedrockCredentials', JSON.stringify(credentials));
     addToast('認証情報を保存しました', NoticeType.success);
   };
 
   const handleReset = () => {
     setCredentials(defaultCredentials);
-    setCredentialsSaved(false);
+    setLLMEnabled(false);
     window.credentialStore.saveKey('bedrockCredentials', '');
     addToast('認証情報の登録をリセットしました', NoticeType.info);
   };
-
-  useEffect(() => {
-    window.credentialStore.isKeySaved('bedrockCredentials').then((exist) => {
-      setCredentialsSaved(exist);
-    });
-  }, []);
 
   return (
     <SettingItem title='Amazon Bedrock利用設定'>
@@ -68,7 +63,7 @@ export function BedrockSetting() {
         <div>
           <h4>アクセスキー設定</h4>
           <div className={styles.accessKeyInputWrapper}>
-            {credentialsSaved ? (
+            {llmEnabled ? (
               <>
                 <InputText
                   value={'********************************'}
@@ -95,7 +90,7 @@ export function BedrockSetting() {
         <div>
           <h4>シークレットキー設定</h4>
           <div className={styles.accessKeyInputWrapper}>
-            {credentialsSaved ? (
+            {llmEnabled ? (
               <>
                 <InputText
                   value={'********************************'}
@@ -122,7 +117,7 @@ export function BedrockSetting() {
         <div>
           <h4>リージョン設定</h4>
           <div className={styles.apiKeyInputWrapper}>
-            {credentialsSaved ? (
+            {llmEnabled ? (
               <DropDownList
                 options={[{ id: '', name: '-' }]}
                 currentValue={'-'}
@@ -140,7 +135,7 @@ export function BedrockSetting() {
         </div>
 
         <div>
-          {credentialsSaved ? (
+          {llmEnabled ? (
             <Button onClick={handleReset} className={styles.apiKeyInputButton}>再設定</Button>
           ) : (
             <Button
