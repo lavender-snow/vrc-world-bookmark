8a6968f0b6bc51c1eabcd00601496585a9f5e5dc
lavender-snow <lavender32192@gmail.com>
2026-01-01 18:32:34 +0900

    [変更] アイコンを変更

--- forge.config.ts
+++ forge.config.ts
@@ -14,7 +14,7 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: 'lavender.icon',
+    icon: 'syuri.icon',
     extraResource: [
       'migrations',
     ],
